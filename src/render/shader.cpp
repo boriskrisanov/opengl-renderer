0929022254d77b14dc96fc75b4a6c90fb4763674
Boris Krisanov <boriskrisanov1@gmail.com>
2023-06-04 09:24:39 +0500

    Run clang-format

--- src/render/shader.cpp
+++ src/render/shader.cpp
@@ -2,17 +2,20 @@
 
 using std::string, std::ifstream, std::vector;
 
-namespace render {
+namespace render
+{
 vector<unsigned int> shaders;
 
-void loadShaderFromFile(std::string sourcePath, ShaderType type) {
+void loadShaderFromFile(std::string sourcePath, ShaderType type)
+{
     DEBUG_LOG("Loading shader: " << sourcePath);
 
     ifstream sourceFile{sourcePath};
     string currentLine;
     string shaderSource;
 
-    while (getline(sourceFile, currentLine)) {
+    while (getline(sourceFile, currentLine))
+    {
         currentLine += "\n";
         shaderSource += currentLine;
     }
@@ -29,7 +32,8 @@ void loadShaderFromFile(std::string sourcePath, ShaderType type) {
     char errorMessage[4096]; // TODO: Variable length error message
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
 
-    if (!success) {
+    if (!success)
+    {
         glGetShaderInfoLog(shader, 4096, nullptr, errorMessage);
         DEBUG_LOG("Shader compilation failed" << errorMessage);
         return;
@@ -38,10 +42,12 @@ void loadShaderFromFile(std::string sourcePath, ShaderType type) {
     shaders.push_back(shader);
 }
 
-unsigned int createShaderProgram() {
+unsigned int createShaderProgram()
+{
     unsigned int shaderProgram = glCreateProgram();
 
-    for (unsigned int shader : shaders) {
+    for (unsigned int shader : shaders)
+    {
         glAttachShader(shaderProgram, shader);
     }
 
@@ -50,7 +56,8 @@ unsigned int createShaderProgram() {
     int success;
     char errorMessage[4096];
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
+    if (!success)
+    {
         glGetProgramInfoLog(shaderProgram, 512, NULL, errorMessage);
         DEBUG_LOG("Shader linking failed: " << errorMessage);
     }
