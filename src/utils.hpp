d837da65c92458eafcd62829d0e826c7bbe5070b
Boris Krisanov <boriskrisanov1@gmail.com>
2024-07-28 15:09:07 +0500

    Check for ENABLE_DEBUG_LOGGING in DEBUG_LOG macro

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,15 +1,19 @@
 #pragma once
 
-#include <vector>
-#include <string>
 #include <chrono>
 #include <iostream>
-#include <optional>
 #include <memory>
+#include <optional>
+#include <string>
+#include <vector>
 
 using std::vector, std::string, std::optional, std::unique_ptr, std::shared_ptr, std::make_shared, std::make_unique;
 
+#ifdef ENABLE_DEBUG_LOGGING
 #define DEBUG_LOG(message) std::cout << "[" << std::chrono::system_clock::now() << "][" << __func__ << "] " << message << "\n"
+#else
+#define DEBUG_LOG(message)
+#endif
 
 [[nodiscard]] std::string loadFile(std::string path);
 [[nodiscard]] std::vector<std::string> splitString(std::string str, std::string delimiter);
